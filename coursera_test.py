7dae9553eb6071ab37b022110e086a252e001aaf
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-08 13:46:56 +0200

    Creating the Code for Course Python for Everyone Excerise 3.2

--- coursera_test.py
+++ coursera_test.py
@@ -1,1 +1,15 @@
-print("Hello World")
+hours = input("How many hours you work: ")
+rate = input("How many you are payed per hour: ")
+try:
+    hrs = float(hours)
+    fra = float(rate)
+except:
+    print("That is not a number")
+    quit()
+
+if (hrs >= 40):
+    pay = 40 * fra
+    pay = pay + ((hrs - 40) * 1.5)
+else:
+    pay = hrs * fra
+print(pay)
