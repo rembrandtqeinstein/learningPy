e30d0a25fb00d63ce60f51f72139f43e37ca01e0
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-08 14:53:47 +0200

    Information Theory basic equations now with user input

--- informationTheory.py
+++ informationTheory.py
@@ -1,7 +1,16 @@
 import math
 
-p = 0.5
+p = input("Input a Probability between 0 and 100: ")
+try:
+    p = float(p)
+except:
+    print("Not a number between 0 and 100")
+    quit()
+
+p = p / 100
+
 entropy = p * (math.log2(1/p))
 shinf = math.log2(1/p)
+eProb = 2**entropy
 
-print(entropy, shinf)
+print(entropy, shinf, eProb)
