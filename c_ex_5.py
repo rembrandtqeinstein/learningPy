2ec14d02e2b8883753c87b262753597cbe4fb387
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-09 14:18:14 +0200

    First excercise solved before video watching

---
+++ c_ex_5.py
@@ -0,0 +1,18 @@
+# Loop Exercise
+# Exercise 1: Write a program which repeatedly reads numbers until the user enters "done". Once "done" is entered, print out the total, count, and average of the numbers. If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
+numb = 0
+suma = 0
+count = 0
+while numb != 'done':
+    numb = input('Enter a number: ')
+    try:
+        numb = int(numb)
+    except:
+        print('bad data')
+    if type(numb) == int:
+        suma = suma + numb
+        count+=1
+
+print(suma, count, (suma/count))
+
+# Exercise 2: Write another program that prompts for a list of numbers as above and at the end prints out both the maximum and minimum of the numbers instead of the average.
