c981955eec5f5e62624d5fc6bbbcbf9417ded2fe
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-11 12:56:36 +0200

    Excercise 7.2 Solved with try and exceptions and sum, count and average of Spam

---
+++ c_ex_7_2.py
@@ -0,0 +1,27 @@
+fname = input('Enter file name and extension: ')
+
+try:
+    fhand = open(fname)
+except:
+    print('Bad file or Bad file Name')
+    quit()
+
+sum = 0
+count = 0
+
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('X-DSPAM-Confidence:'):
+        continue
+    else:
+        pos = line.find(':')
+        try:
+            spamScore = float(line[pos+1:])
+        except:
+            print('Not Numbers')
+        sum = sum + spamScore
+        count = count + 1
+        #print(line)
+        #print(sum, count)
+
+print(sum, count, sum/count)
