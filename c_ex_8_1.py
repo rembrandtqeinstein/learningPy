dd885c58b459e53014fed4a3f12356eae380c4f3
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-12 20:40:37 +0200

    Excercise 8.1 of List Coursera. Read a file (romeo.txt) iterate through the words by splitting it, and appending if the word is not present already into a list

---
+++ c_ex_8_1.py
@@ -0,0 +1,20 @@
+fname = input("Please provide the name of the file with the extension: ")
+
+try:
+    fhandler = open(fname)
+except:
+    print("Bad file name, please input valid, like romeo.txt")
+
+words = list()
+
+for line in fhandler:
+    line = line.rstrip()
+    line = line.split()
+    for word in line:
+        if word not in words:
+            words.append(word)
+        else:
+            continue
+
+words.sort()
+print(words)
