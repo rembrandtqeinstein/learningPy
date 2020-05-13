e755ba18f59e7705dcdb37a231b4b7a9cfa88e17
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-13 11:27:55 +0200

    Excercise 8.3 from Coursera P4E on which user is promted to input numbers, then added to the list, and then obtain the min and max number, with try and except for not numeric numbers

---
+++ c_ex_8_3.py
@@ -0,0 +1,13 @@
+flist = list()
+while True:
+    numb = input("Say a number: ")
+    if numb == 'done' : break
+    try:
+        numb = float(numb)
+    except:
+        print('Its not a number')
+        continue
+    flist.append(numb)
+
+print('Max number inputted',max(flist))
+print('Min number inputted',min(flist))
