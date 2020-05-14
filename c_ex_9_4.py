355b1c24f18ea1be9a2202fec6c54418392b649d
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-14 11:13:59 +0200

    Coursera Python 4 Everyone Excercise 9.4 solution that takes the email domain of the sender and not the whole email

---
+++ c_ex_9_4.py
@@ -0,0 +1,27 @@
+fname = input('Enter a valid file name and extension: ')
+
+try:
+    fhandler = open(fname)
+except:
+    print('Not a valid file, try mbox-short.txt')
+    quit()
+
+hist = dict()
+
+for line in fhandler:
+    if not line.startswith('From:'):
+        continue
+    words = line.split()
+    # This saves into Words only a list of splitted Email based on the @
+    words = words[1].split('@')
+    hist[words[1]] = hist.get(words[1],0) + 1
+
+print(hist)
+bcount = None
+bname = None
+
+for v,k in hist.items():
+    if bname == None or bcount < k:
+        bcount = k
+        bname = v
+print(bname, bcount)
