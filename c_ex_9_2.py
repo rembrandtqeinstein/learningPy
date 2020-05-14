94feec10db00b3a2bcdee3800f50f0b7ddfb29a2
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-14 10:20:58 +0200

    Python 4 Everyone Excercise 9.2 from Chapter of Dictionaries

---
+++ c_ex_9_2.py
@@ -0,0 +1,18 @@
+fname = input('Enter a valid file name and extension: ')
+
+try:
+    fhandler = open(fname)
+except:
+    print('Not a valid file, try mbox-short.txt')
+    quit()
+
+hist = dict()
+
+for line in fhandler:
+    if not line.startswith('From:'):
+        continue
+    words = line.split()
+    # This only works because we know emails are on the second position
+    hist[words[1]] = hist.get(words[1],0) + 1
+
+print(hist)
