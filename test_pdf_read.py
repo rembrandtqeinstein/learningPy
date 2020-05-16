dba13a6eb59cd8ed758c3d983108433433c41a84
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-16 22:40:51 +0200

    PDF Reader with PDFPLUMBER to count the incidence of words inside the PDF

--- test_pdf_read.py
+++ test_pdf_read.py
@@ -2,5 +2,25 @@ import pdfplumber
 pdf = pdfplumber.open('pythonlearn.pdf')
 page = pdf.pages[3]
 text = page.extract_text()
-print(text)
+#count = 0
+
+for page in pdf.pages:
+    text = text + page.extract_text()
+    #count = count +1
+    #print(text)
+
+hist = dict()
+#print(text)
+#print(len(pdf.pages))
+
+for w in text:
+    w = w.lower()
+    if w.isalpha():
+        hist[w] = hist.get(w, 0) + 1
+    #print(w)
+ord = sorted([(v,k) for k,v in hist.items()], reverse=True)
+for v,k in ord:
+    print(k,v)
+
 pdf.close()
+
