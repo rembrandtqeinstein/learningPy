ee197c909c2da5e134f738bc510e23a9a2d2427d
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-22 17:39:07 +0200

    Solution to excercise 12.3 from P4E

--- c_ex_12_3.py
+++ c_ex_12_3.py
@@ -1,11 +1,17 @@
-import urllib.request, urllib.parse, urllib.error
 
-fhand2 = urllib.request.urlopen('http://data.pr4e.org/intro.txt')
-count = 0
+import urllib.request
+import urllib.parse
+import urllib.error
 
-for line2 in fhand2:
-    words = line2.decode().split()
-    for word in words:
-        for ch in word:
-            count = count + 1
-print(count)
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/intro.txt')
+
+characters = 0
+for line in fhand:
+    # \n is considered a character
+    # Amend to line.decode().rstrip() if needed
+    words = line.decode()
+    characters = characters + len(words)
+    if characters < 3000:
+        print(line.decode().strip())
+print(characters)
