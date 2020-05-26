50bfb85f160ad49ff8be963acad87b5516ba2531
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-26 15:55:57 +0200

    minor fixes and comments

--- c_ex_13_1.py
+++ c_ex_13_1.py
@@ -4,10 +4,13 @@ import xml.etree.ElementTree as ET
 url = input('Enter -')
 # Read the entered URL
 html = urllib.request.urlopen(url).read()
+# Convert it to a String
 fun = html.decode()
 
 tot = 0
+# Get the Tree Object from the String
 tree = ET.fromstring(fun)
+# Find all the Counts in the XML
 results = tree.findall('.//count')
 for n in results:
     count = int(n.text)
