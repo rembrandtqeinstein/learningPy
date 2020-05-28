99d480768552dc837db724f0e14354d7270e226a
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-28 09:56:07 +0200

    Keys removed

--- code3/hidden.py
+++ code3/hidden.py
@@ -5,7 +5,7 @@
 #https://developer.twitter.com/en
 
 def oauth():
-    return {"consumer_key": "W13nU8Rr3WSXVGgKmPPQ",
-            "consumer_secret": "DC8XYI97b7qbKhT0eHyJdOfu3VPevXKCWFl4gwUMqM",
-            "token_key": "130585483-pO13BRbTxNRbxIom6UvAaFhNpEcH5I07L6bX034w",
-            "token_secret": "p4oMGwmmFyzDjBrICCSU0rnPTlfmxbvbWe9V02IdQydHl"}
+    return {"consumer_key": "REPLACE",
+            "consumer_secret": "REPLACE",
+            "token_key": "REPLACE",
+            "token_secret": "REPLACE"}
