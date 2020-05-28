d4a81f08b031ffd5421368b05372523a5c1cc3cd
Hercules Rockefeller <hernan.herrera.hall@gmail.com>
2020-05-28 09:39:30 +0200

    Token and Secrets updated but non functional

--- code3/hidden.py
+++ code3/hidden.py
@@ -2,9 +2,10 @@
 
 # https://apps.twitter.com/
 # Create new App and get the four strings
+#https://developer.twitter.com/en
 
 def oauth():
-    return {"consumer_key": "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key": "W13nU8Rr3WSXVGgKmPPQ",
+            "consumer_secret": "DC8XYI97b7qbKhT0eHyJdOfu3VPevXKCWFl4gwUMqM",
+            "token_key": "130585483-pO13BRbTxNRbxIom6UvAaFhNpEcH5I07L6bX034w",
+            "token_secret": "p4oMGwmmFyzDjBrICCSU0rnPTlfmxbvbWe9V02IdQydHl"}
