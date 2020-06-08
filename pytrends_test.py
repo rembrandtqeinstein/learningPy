2d7714d5580687b065ea6f45cac04732070dfcfd
hernanherrera <hernan.herrera.hall@gmail.com>
2020-06-08 21:33:04 +0200

    Daily PyTrends

--- pytrends_test.py
+++ pytrends_test.py
@@ -20,8 +20,8 @@ pytrends = TrendReq(hl='en-US', tz=360)
 #fig = image.get_figure()
 #fig.savefig('figure.png')
 
-df = pytrends.trending_searches(pn='argentina')
-print(df)
+#df = pytrends.trending_searches(pn='argentina')
+#print(df)
 
 #t = pytrends.get_historical_interest('Corona', year_start=2020, month_start=1, day_start=1, hour_start=0, year_end=2020, month_end=5, day_end=1, hour_end=0, cat=0, geo='', gprop='', sleep=20)
 #print(t)
