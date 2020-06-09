ecc0cacedf4b34acbc4ddf8d469b2ddd2debc696
hernanherrera <hernan.herrera.hall@gmail.com>
2020-06-09 15:56:50 +0200

    Python Practice Excercise 9 Solution https://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html

---
+++ pp_e_9.py
@@ -0,0 +1,24 @@
+import random
+
+while True:
+    count = 0
+    num = random.randrange(1,10)
+    guess = None
+    while guess == None:
+        gs = input("Guess a number from 1 to 9: ")
+        if gs == 'exit':
+            exit()
+        try:
+            gs = int(gs)
+        except:
+            print("Not a number")
+        #if gs > 9 or gs < 1:
+        #    print("Number out of range")
+        if gs == num:
+            guess = True
+            print(gs, "is the number. You guessed on your", count + 1)
+        elif gs > num:
+            print(gs, "is higher than the number")
+        elif gs < num:
+            print(gs, "is lower than the number")
+        count = count + 1
