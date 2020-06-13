aa7852e5e140db739ba90c33302a7114a76b9f77
hernanherrera <hernan.herrera.hall@gmail.com>
2020-06-13 13:23:21 +0200

    Python Practice Excercise 22 Solution https://www.practicepython.org/exercise/2014/12/06/22-read-from-file.html

--- pp_e_22.py
+++ pp_e_22.py
@@ -11,3 +11,16 @@ ord = sorted([(v,k) for k,v in dic.items()], reverse=True)
 
 for v,k in ord:
     print(k,v)
+
+file2 = open("Training_01.txt", 'r')
+dic2 = dict()
+
+for i in file2:
+    names = i.rstrip()
+    names = i.split("/")
+    dic2[names[2]] = dic2.get(names[2], 0) + 1
+
+ord2 = sorted([(v,k) for k,v in dic2.items()], reverse=True)
+
+for v,k in ord2:
+    print(k,v)
