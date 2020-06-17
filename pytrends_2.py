54de5bbfe213af4b3692d009d73f938fc51a7473
hernanherrera <hernan.herrera.hall@gmail.com>
2020-06-17 14:30:21 +0200

    PyTrends Excercise and URL for catching Google Trend Searches

---
+++ pytrends_2.py
@@ -0,0 +1,12 @@
+#https://predictivehacks.com/get-google-trends-using-python/
+import pandas as pd #pandas 0.25
+from pytrends.request import TrendReq
+
+pytrend = TrendReq()
+
+trending_searches_df = pytrend.trending_searches()
+print(trending_searches_df.head(20))
+
+# Get Google Hot Trends data
+today_searches_df = pytrend.today_searches()
+print(today_searches_df.head(20))
