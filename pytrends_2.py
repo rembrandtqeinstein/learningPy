848f8fe6955d2ecbfb22ed4cfa76820e95db7f3c
hernanherrera <hernan.herrera.hall@gmail.com>
2020-06-24 15:27:09 +0200

    New Code to Plot comparison Words with the PyTrend API from Google like this Article states: https://medium.com/@yanweiliu/getting-the-google-trends-data-with-python-67b335e7d1cf Added the Request List of Words to compare on the Final part of the Code and added comments

--- pytrends_2.py
+++ pytrends_2.py
@@ -4,26 +4,31 @@ from pytrends.request import TrendReq
 
 pytrend = TrendReq()
 
+print("Trending Searches WorldWide")
 trending_searches_df = pytrend.trending_searches()
 print(trending_searches_df)
 
 print()
-
+print("Get Today Trending Searches")
 # Get Google Hot Trends data
 today_searches_df = pytrend.today_searches()
 print(today_searches_df.head(20))
 
 print()
-
-kw_list = ["Podcast"]
+print("See Related Queries to a Particular Query o Queries (Suggested to use just one")
+#kw_list = ["Podcast"]
+kw_list = list()
+kw = input("Enter the Keywords you want to look up separated by a ,: ")
+kw = kw.split(",")
+for word in kw:
+    kw_list.append(word)
+print(kw_list)
 tb = pytrend.build_payload(
     kw_list,
     cat=0,
-    timeframe='today 5-y',
-    geo='ES',
+    timeframe='today 5-y', # This can be changed to have a new timeline
+    geo='ES', # This needs to change if you want not to use Spain
     gprop='')
-print()
-
 rq = pytrend.related_queries()
 #rq = rq.drop(labels=['isPartial'],axis='columns')
 top = rq.get('Podcast').get('top')
@@ -33,21 +38,26 @@ print()
 print("Top Trending Queries related to: {}".format(kw_list))
 rising = rq.get('Podcast').get('rising')
 print(rising)
-#for k,v in rq.items():
-#    print(k, v)
 
+print()
+print("Interest Over Time of the befored Selected Queries")
 iot = pytrend.interest_over_time()
 iot = iot.drop(labels=['isPartial'],axis='columns')
 img = iot.plot(title = 'Comparing the words: {}'.format(kw_list))
 fig = img.get_figure()
 fig.savefig('figureP.png')
 
-
-
-
-from pytrends.request import TrendReq
+print()
+print("Comparative Graph creation and CSV File for Compared Keywords")
 pytrend = TrendReq(tz=360)
-keywords = ['Podcast', 'Radio']
+#keywords = ['Podcast', 'Radio']
+keywords = list()
+str = input("Enter the Keywords you want to search for separated by a ,: ")
+str = str.split(",")
+for word in str:
+    keywords.append(word)
+print(keywords)
+
 pytrend.build_payload(
      kw_list=keywords,
      cat=0,
