e5f990f74b2d27d2a8993344d83b0bc807f40175
hernanherrera <hernan.herrera.hall@gmail.com>
2020-06-28 09:57:50 +0200

    Updated Temp Graphs

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ import datetime
 import numpy as np
 
 start = datetime.datetime.strptime("06-05-2020", "%d-%m-%Y")
-end = datetime.datetime.strptime("27-06-2020", "%d-%m-%Y")
+end = datetime.datetime.strptime("28-06-2020", "%d-%m-%Y")
 date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 dl = list()
 
@@ -13,7 +13,7 @@ for date in date_generated:
 temp = [35.3, 35.3, 35.8, 35.5, 35.6, 35.2, 35.4, 35.7, 35, 35.7, 36.1, 35.8, 35.8, 35.3, 35.3,
         36.1, 36.0, 35.3, 35.9, 35.7, 36.0, 35.8, 36.3, 35.9, 35.7, 35.4, 35.3, 36.0, 35.5, 36.1,
         36.1, 35.8, 35.3, 35.3, 35.3, 35.3, 35.4, 36.0, 35.7, 35.4, 35.0, 35.8, 35.1, 35.3, 35.7,
-        36.1,35.9, 35.7, 35.2, 35.9, 34.9, 35.7]
+        36.1,35.9, 35.7, 35.2, 35.9, 34.9, 35.7, 35.9]
 
 counts = dict()
 for x in temp:
