29b03c49a30ffac4ff5d1bbeeaee1c65a9dac14a
hernan.herrera.hall@gmail.com <Wunderman11!>
2020-07-09 08:14:13 +0200

    Updated Weight 8-Jul

--- graph_w.py
+++ graph_w.py
@@ -3,14 +3,14 @@ import datetime
 import statistics
 
 start = datetime.datetime.strptime("07-07-2020", "%d-%m-%Y")
-end = datetime.datetime.strptime("09-07-2020", "%d-%m-%Y")
+end = datetime.datetime.strptime("10-07-2020", "%d-%m-%Y")
 date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 dl = list()
 
 for date in date_generated:
     dl.append(date.strftime("%d-%m-%Y"))
 
-weight = [67.1, 66.8]
+weight = [67.1, 66.8, 66.8]
 
 counts = dict()
 for x in weight:
@@ -30,4 +30,4 @@ for k, v in ord:
 print("Average weight rounded till {}".format(end))
 print(round(statistics.mean(weight),1))
 print("Median weight rounded till {}".format(end))
-print(statistics.median(weight),1)
+print(statistics.median(weight))
