bc8c4a49562cbd7fcc7b51e93783723ed0cd6d57
hernan.herrera.hall@gmail.com <Wunderman11!>
2020-07-13 10:49:51 +0200

    Updated T Graph 13-Jul

--- graph_w.py
+++ graph_w.py
@@ -1,22 +1,36 @@
 import matplotlib.pyplot as plt
 import datetime
 import statistics
+import numpy as np
 
 start = datetime.datetime.strptime("07-07-2020", "%d-%m-%Y")
-end = datetime.datetime.strptime("13-07-2020", "%d-%m-%Y")
+end = datetime.datetime.strptime("14-07-2020", "%d-%m-%Y")
 date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 dl = list()
 
 for date in date_generated:
     dl.append(date.strftime("%d-%m-%Y"))
 
-weight = [67.1, 66.8, 66.8, 66.5, 66.7, 67.6]
+weight = [67.1, 66.8, 66.8, 66.5, 66.7, 67.6, 68.2]
 
 counts = dict()
 for x in weight:
     counts[x] = counts.get(x,0) + 1
 
-plt.plot(dl, weight)
+for x,y in zip(dl,weight):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xticks(np.arange(0,10,1))
+plt.yticks(np.arange(0,7,0.5))
+
+plt.plot(dl, weight, 'bo-')
 plt.show()
 
 plt.hist(weight, bins=len(weight))
