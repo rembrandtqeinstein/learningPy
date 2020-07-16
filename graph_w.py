b6af54970e57ff748cff15650a8f31c84bd22d8c
hernan.herrera.hall@gmail.com <Wunderman11!>
2020-07-16 10:11:04 +0200

    Updated T&W Graph 16-July

--- graph_w.py
+++ graph_w.py
@@ -4,14 +4,14 @@ import statistics
 import numpy as np
 
 start = datetime.datetime.strptime("07-07-2020", "%d-%m-%Y")
-end = datetime.datetime.strptime("16-07-2020", "%d-%m-%Y")
+end = datetime.datetime.strptime("17-07-2020", "%d-%m-%Y")
 date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 dl = list()
 
 for date in date_generated:
     dl.append(date.strftime("%d-%m-%Y"))
 
-weight = [67.1, 66.8, 66.8, 66.5, 66.7, 67.6, 68.2, 67.5, 67.8]
+weight = [67.1, 66.8, 66.8, 66.5, 66.7, 67.6, 68.2, 67.5, 67.8, 67.9]
 
 counts = dict()
 for x in weight:
