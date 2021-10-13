e7e9704c419ba9858139de056a0d83215b7f4949
hernan.herrera.hall@gmail.com <Wunderman11!>
2021-10-13 14:54:12 +0200

    Test

--- pytrends_2.py
+++ pytrends_2.py
@@ -74,7 +74,7 @@ print(keywords)
 pytrend.build_payload(
      kw_list=keywords,
      cat=0,
-     timeframe='today 12-m',
+     timeframe='today 12-',
      geo='',
      gprop='')
 data = pytrend.interest_over_time()
